72211d1dc23f732f6294e2f4cb50e158c02fffdd
akpalo <akseli.palo@student.lab.fi>
2024-10-15 10:51:36 +0300

    Eslint konfigurointia

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const eslintPluginPrettier = require("eslint-plugin-prettier");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettier.recommended
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+      eslintPluginPrettier.recommended
+    ],
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          "parser": "angular"
+        },
+      ],
+    },
+  }
+);
