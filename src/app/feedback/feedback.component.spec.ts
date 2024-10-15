066f2ad86cc6c4134cbb8fb1451dc78b3ee06a28
akpalo <akseli.palo@student.lab.fi>
2024-10-15 16:57:18 +0300

    Yksikkötestit luotu

--- src/app/feedback/feedback.component.spec.ts
+++ src/app/feedback/feedback.component.spec.ts
@@ -20,4 +20,29 @@ describe('FeedbackComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // 1.Testi - Lomake on virheellinen, jos nimi - kenttä on tyhjä
+  it('Should be invalid when required fields are empty', () => {
+    component.fbForm.controls['name'].setValue('');
+    // Tähän voidaan lisätä lisää kenttiä, jos halutaan tarkistaa niitä enemmän
+    expect(component.fbForm.controls['name'].invalid).toBeTruthy();
+    // Muoto, jos halutaan tarkastaa kaikki:
+    // expect(component.fbForm.invalid).toBeTruthy();
+  })
+
+  // 2.Testi - sähköpostin muotoilun tarkistus
+  it('Should show email error when email format is incorrect', () => {
+    component.fbForm.controls['email'].setValue('invalid-email');
+    expect(component.fbForm.controls['email'].hasError('email')).toBeTruthy();
+  })
+
+  // 3.Testi - Lomakkeen alustamisen tarkistus
+  it('should create the form with 6 controls', () => {
+    expect(component.fbForm.contains('title')).toBeTruthy();
+    expect(component.fbForm.contains('description')).toBeTruthy();
+    expect(component.fbForm.contains('name')).toBeTruthy();
+    expect(component.fbForm.contains('email')).toBeTruthy();
+    expect(component.fbForm.contains('phone')).toBeTruthy();
+    expect(component.fbForm.contains('termsAndConditions')).toBeTruthy();
+  });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: Router;
 
   beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']); // Luodaan routerille spy
+
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      providers: [{ provide: Router, useValue: routerSpy }], // käytetään spytä
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
   // Valmis testi
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // 4.Testi - testataan, viekö painike feedback sivulle
+  it('Should navigate to feedback page when button is clicked', () => {
+    const navigateSpy = router.navigate as jasmine.Spy; // Spy routerin navigointiin
+    const button = fixture.debugElement.query(By.css('button')); // Etsitään painike HTML - puolelta
+    button.triggerEventHandler('click', null); // klikkauksen simulointi
+    fixture.detectChanges(); // Päivitetään näkymä
+    expect(navigateSpy).toHaveBeenCalledWith(['feedback']); // tarkistetaan, että navigointi toimii
+  });
+
+  // 5.testi - testataan, toimivatko linkit
+  it('should have correct links in the home page', () => {
+    const links = fixture.nativeElement.querySelectorAll('a');
+    expect(links.length).toBe(3); // Tarkistaa, että linkkejä on kolme
+
+  expect(links[0].getAttribute('href')).toBe('https://angular.io/guide/testing-components-scenarios');
+  expect(links[1].getAttribute('href')).toBe('https://testing-angular.com/introduction/#introduction');
+  expect(links[2].getAttribute('href')).toBe('https://simpleweblearning.com/form-testing-in-angular/');
+  })
 });
